5aecf02bd8b99048d0ad56ec18635a1354fd8178
Peng Li <963747613@qq.com>
2022-03-02 11:12:31 +0800

    feat: complete the location when mars rover init

---
+++ src/rover.js
@@ -0,0 +1,10 @@
+export default class Rover {
+
+    constructor(location) {
+        this.location = location;
+    }
+
+    getLocation() {
+        return this.location;
+    }
+}
--- src/rover.test.js
+++ src/rover.test.js
@@ -3,6 +3,6 @@ import Rover from './rover'
 describe('Mars Rover', () => {
     it('Mars Rover should return a rover', () => {
         const rover = new Rover({x:1,y:1,direction:'N'});
-        expect(rover.getLocation()).toBe({ x: 1, y: 1, direction: 'N' });
+        expect(rover.getLocation()).toEqual({ x: 1, y: 1, direction: 'N' });
     })
 })
